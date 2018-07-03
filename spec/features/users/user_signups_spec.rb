4de5b0c93845b53c0200a47471e23c567ed0f439
Nurulazrad M <nurulazrad@gmail.com>
2018-07-03 14:04:37 +0800

    Add user validations for first and last name

--- spec/features/users/user_signups_spec.rb
+++ spec/features/users/user_signups_spec.rb
@@ -18,4 +18,19 @@ RSpec.feature "Users signup", type: :feature do
 
     expect(page).to have_content 'John Doe'
   end
+
+  scenario "with invalid credentials" do
+    visit root_path
+
+    click_link "Sign up"
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "secret"
+    fill_in "Password confirmation", with: "secret"
+    click_button "Sign up"
+
+    expect(page).to have_content "First name can't be blank"
+    expect(page).to have_content "Last name can't be blank"
+  end
 end
